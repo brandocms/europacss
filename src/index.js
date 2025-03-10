141d2968faab244293984a18d62e44bb0860485c
T. Mjoen <trond@twined.net>
2025-03-10 11:24:23 +0100

    Update index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _ from 'lodash'
 
-import postcss from 'postcss'
 import postcssNested from 'postcss-nested'
 import postcssExtend from 'postcss-extend-rule'
 
@@ -24,37 +23,28 @@ const getConfigFunction = config => () => {
   return resolveConfig([_.isObject(config) ? config : require(config), defaultConfig])
 }
 
-const plugin = postcss.plugin('europacss', config => {
+module.exports = config => {
   const resolvedConfigPath = resolveConfigPath(config)
   const cfgFunction = getConfigFunction(resolvedConfigPath || config)
 
-  const preludium = [
-    substituteRowAtRules(cfgFunction),
-    postcssNested(),
-    postcssExtend()
-  ]
+  const preludium = [substituteRowAtRules(cfgFunction), postcssNested(), postcssExtend()]
 
-  const postludium = [
-    postcssNested(),
-    formatCSS
-  ]
+  const postludium = [postcssNested(), formatCSS]
 
-  const configuredEuropaPlugins = plugins.map(plug => plug(cfgFunction))
+  const configuredEuropaPlugins = plugins.map(plug => {
+    return plug(cfgFunction)
+  })
 
-  const pipeline = [
-    ...preludium,
-    ...configuredEuropaPlugins,
-    ...postludium
-  ]
+  const pipeline = [...preludium, ...configuredEuropaPlugins, ...postludium]
 
   if (!_.isUndefined(resolvedConfigPath)) {
     pipeline.push(registerConfigAsDependency(resolvedConfigPath))
   }
 
-  return (root, result) => pipeline.reduce(
-    (promise, plugin) => promise.then(
-      () => plugin(result.root, result)
-    ), Promise.resolve())
-})
+  return {
+    postcssPlugin: 'europacss',
+    plugins: [...pipeline]
+  }
+}
 
-module.exports = plugin
+module.exports.postcss = true
